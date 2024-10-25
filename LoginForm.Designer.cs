406ca8dbe9316f10d964471e98a8895c409caa5f
Vlad <v.redula@faradaygroup.com>
2024-10-25 18:12:20 +0900

    Adding loginform

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace QuoToPO
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            usernameLbl = new Label();
+            passwordLbl = new Label();
+            btnLogin = new Button();
+            linkLabel1 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(78, 23);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(213, 23);
+            txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(78, 64);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(213, 23);
+            txtPassword.TabIndex = 1;
+            // 
+            // usernameLbl
+            // 
+            usernameLbl.AutoSize = true;
+            usernameLbl.Location = new Point(12, 26);
+            usernameLbl.Name = "usernameLbl";
+            usernameLbl.Size = new Size(60, 15);
+            usernameLbl.TabIndex = 2;
+            usernameLbl.Text = "Username";
+            // 
+            // passwordLbl
+            // 
+            passwordLbl.AutoSize = true;
+            passwordLbl.Location = new Point(12, 67);
+            passwordLbl.Name = "passwordLbl";
+            passwordLbl.Size = new Size(57, 15);
+            passwordLbl.TabIndex = 3;
+            passwordLbl.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(12, 118);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(279, 39);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(103, 160);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(100, 15);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Forgot password?";
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(303, 185);
+            Controls.Add(linkLabel1);
+            Controls.Add(btnLogin);
+            Controls.Add(passwordLbl);
+            Controls.Add(usernameLbl);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            Load += LoginForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Label usernameLbl;
+        private Label passwordLbl;
+        private Button btnLogin;
+        private LinkLabel linkLabel1;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,43 @@
+﻿namespace QuoToPO
+{
+    public partial class LoginForm : Form
+    {
+        private DatabaseHelper _db;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+
+            // Initialize DatabaseHelper
+            _db = new DatabaseHelper();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (_db.AuthenticateUser(username, password))
+            {
+                // Authentication successful
+                MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Hide the login form and show the main application form
+                this.Hide();
+                MainForm mainForm = new MainForm();
+                mainForm.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                // Authentication failed
+                MessageBox.Show("Invalid username or password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
