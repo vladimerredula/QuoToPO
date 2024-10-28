251395d3f3e4e145a50811e384e360a0a9a93d32
Vlad <v.redula@faradaygroup.com>
2024-10-28 18:04:00 +0900

    Added company class

---
+++ Company.cs
@@ -0,0 +1,16 @@
+﻿namespace QuoToPO
+{
+    public class Company
+    {
+        // Properties for company
+        public int Company_ID { get; set; }
+        public string Company_name { get; set; }
+        public string Company_name_jpn { get; set; }
+        public string Company_address { get; set; }
+        public string Company_address_jpn { get; set; }
+        public string Postal_code { get; set; }
+        public string Telephone { get; set; }
+        public string Fax { get; set; }
+        public string Key_words { get; set; }
+    }
+}
