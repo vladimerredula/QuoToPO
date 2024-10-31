7553a873e49a8f4a792393a8e23abab969488c36
Vlad <v.redula@faradaygroup.com>
2024-10-31 15:39:19 +0900

    Adding PdfType classs

---
+++ PdfType.cs
@@ -0,0 +1,18 @@
+﻿namespace QuoToPO
+{
+    internal class PdfType
+    {
+        // Properties for user
+        public int Pdf_type_ID { get; set; }
+        public string Pdf_type_name { get; set; }
+
+        public List<PdfType> GetList()
+        {
+            return new List<PdfType>()
+        {
+            new PdfType { Pdf_type_ID = 1, Pdf_type_name = "Native PDF" },
+            new PdfType { Pdf_type_ID = 2, Pdf_type_name = "Scanned PDF" }
+        };
+        }
+    }
+}
